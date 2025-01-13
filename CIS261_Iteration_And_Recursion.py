a2e7324c427760ea09017ddbfae708bdb6798742
JacobKessler <Javadotdet@gmail.com>
2025-01-13 16:21:52 -0500

    added global code to main func

--- CIS261_Iteration_And_Recursion.py
+++ CIS261_Iteration_And_Recursion.py
@@ -11,15 +11,20 @@ def factorial_recur(n: int):
 	return n * factorial_recur(n - 1)
 
 
-numbers = [
-		0, 5, 10, 25, 50, 100
-]
+def main():
+	numbers = [
+			0, 5, 10, 25, 50, 100
+	]
 
-print("Iterative factorial:")
-for n in numbers:
-	print(f"{n}! = {factorial_iter(n)}")
+	print("Iterative factorial:")
+	for n in numbers:
+		print(f"{n}! = {factorial_iter(n)}")
 
-print()
-print("Recursive factorial:")
-for n in numbers:
-	print(f"{n}! = {factorial_recur(n)}")
+	print()
+	print("Recursive factorial:")
+	for n in numbers:
+		print(f"{n}! = {factorial_recur(n)}")
+
+
+if __name__ == '__main__':
+	main()
