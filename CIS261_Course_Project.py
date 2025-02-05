477b1fe931178eee2e847e459d1b01be40b619b6
JacobKessler <Javadotdet@gmail.com>
2025-02-05 17:41:15 -0500

    Week 5 commit and bugfix

--- CIS261_Course_Project.py
+++ CIS261_Course_Project.py
@@ -29,7 +29,7 @@ def rate(hourly_rate: str):
 	if not hourly_rate.isnumeric():
 		print("INVALID RATE AMOUNT")
 		return 0
-	print(f"Hours set to {hourly_rate}")
+	print(f"Rate set to {hourly_rate}")
 	data[ACTIVE_EMPLOYEE]["rate"] = int(hourly_rate)
 	return data[ACTIVE_EMPLOYEE]["rate"]
 
@@ -66,13 +66,24 @@ def everything_input(hours: str, hourly_rate: str, tax: str):
 	print(f"Gross pay: {gross}\nNet pay: {net}")
 
 
-def display():
+def display(conclusion = False):
 	if not ACTIVE_EMPLOYEE:
-		print("NO ACTIVE EMPLOYEE, USE NAME COMMAND!")
+		if not conclusion:
+			print("NO ACTIVE EMPLOYEE!")
 		return
 	print(f"{ACTIVE_EMPLOYEE}:")
 	print("\n".join(
-			f"\t{k}: {v}" for k, v in data[ACTIVE_EMPLOYEE].items()
+			f"\t{k.capitalize()}: {v}" for k, v in data[ACTIVE_EMPLOYEE].items()
+	))
+	gross_pay = data[ACTIVE_EMPLOYEE].get("hours", 0.0) * data[ACTIVE_EMPLOYEE].get("rate", 0.0)
+	tax = data[ACTIVE_EMPLOYEE].get("tax", 0.0)
+	net_pay = (1.0 - tax) * gross_pay
+
+	print("\n".join(
+			f"{k}: {v}" for k, v in {
+					"Gross pay": gross_pay,
+					"Net pay":   net_pay
+			}.items()
 	))
 
 
@@ -88,8 +99,34 @@ def count():
 					"Net pay":   sum(
 							value.get("hours", 0) * value.get("rate", 0) * (1.0 - value.get("tax", 0)) for value in
 							data.values())
-			}.values()
+			}.items()
 	))
+
+
+def date_from(month_str: str, day_str: str, year_str: str):
+	if not ACTIVE_EMPLOYEE:
+		print("NO ACTIVE EMPLOYEE, USE NAME COMMAND!")
+		return
+	if not month_str.isnumeric() or not day_str.isnumeric() or not year_str.isnumeric():
+		print("INVALID ARGUMENT(S)")
+		return
+	date = f"{month_str}/{day_str}/{year_str}"
+	data[ACTIVE_EMPLOYEE]["start date"] = date
+	print("Start date set to " + date)
+	return date
+
+
+def date_to(month_str: str, day_str: str, year_str: str):
+	if not ACTIVE_EMPLOYEE:
+		print("NO ACTIVE EMPLOYEE, USE NAME COMMAND!")
+		return
+	if not month_str.isnumeric() or not day_str.isnumeric() or not year_str.isnumeric():
+		print("INVALID ARGUMENT(S)")
+		return
+	date = f"{month_str}/{day_str}/{year_str}"
+	data[ACTIVE_EMPLOYEE]["end date"] = date
+	print("End date set to " + date)
+	return date
 
 
 def main():
@@ -100,7 +137,9 @@ def main():
 			"tax":        [1, taxes],
 			"calculator": [3, everything_input],
 			"display":    [0, display],
-			"count":      [0, count]
+			"count":      [0, count],
+			"start_date": [3, date_from],
+			"end_date":   [3, date_to]
 	}
 	while True:
 		value = input("Enter Command:\t").strip()
@@ -110,6 +149,7 @@ def main():
 			args = value[1:]
 			value = value[0].lower()
 		if value == "end":
+			display(True)
 			break
 		elif value not in functions:
 			print("INVALID COMMAND!")
